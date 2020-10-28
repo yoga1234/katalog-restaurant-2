02db33bfda71b92b5692181b4350bd04590d5cbe
Septiana Yoga <septianayoga30@gmail.com>
2020-10-28 12:59:29 +0700

    updating another shit

--- src/scripts/utils/page-loader.js
+++ src/scripts/utils/page-loader.js
@@ -5,33 +5,14 @@ import { checkFavoriteOnDb } from './favorite-restaurant-initiator'
 import { backToTopBehaviour } from './scroll-top'
 import { getDetailRestaurant } from '../data/restaurant-data-source'
 
-const paginationListener = () => {
-  const pageInformation = {
-    activePage: 1,
-    pageDestination: 1
-  }
-  const paginationPage = document.querySelectorAll('.pagination-page')
-
-  for (let i = 0; i < paginationPage.length; i++) {
-    paginationPage[i].addEventListener('click', async function (e) {
-      e.preventDefault()
-      pageInformation.pageDestination = parseInt(paginationPage[i].innerHTML)
-      pageInformation.activePage = parseInt(document.querySelector('.active-page').innerHTML)
-      pageLoader(pageInformation)
-    })
-  }
-}
-
-const pageLoader = async (pageInformation) => {
+const pageLoader = async () => {
   const mainContent = document.getElementById('maincontent')
-
-  pageInformation = pageInformation || { activePage: 1, pageDestination: 1 }
 
   skipToContent()
 
   mainContent.innerHTML = await urlParser.loadPage('empty')
-  mainContent.innerHTML = await urlParser.loadPage(pageInformation)
-  if (window.location.hash !== '#home' && window.location.hash !== '#favorite') {    
+  mainContent.innerHTML = await urlParser.loadPage()
+  if (window.location.hash !== '#home' && window.location.hash !== '#favorite') {
     const idDetail = window.location.href.split('/')[4]
     // getting data for saving into database
     const restaurantData = await getDetailRestaurant(idDetail)
@@ -39,7 +20,6 @@ const pageLoader = async (pageInformation) => {
 
     backToTopBehaviour()
   }
-  paginationListener()
 }
 
 export default pageLoader

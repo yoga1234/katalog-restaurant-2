1b9c373211ab7070b7f2f88cff89e1b31758db25
Septiana Yoga <septianayoga30@gmail.com>
2020-10-30 14:07:09 +0700

    updating recomendation from rejected work

--- src/scripts/sw.js
+++ src/scripts/sw.js
@@ -1,16 +1,23 @@
-// import 'regenerator-runtime'
-// import cacheHelper from './utils/cache-helper'
+import 'regenerator-runtime'
+import cacheHelper from './utils/cache-helper'
 
-// const { assets } = global.serviceWorkerOption
+const { assets } = global.serviceWorkerOption
 
-// self.addEventListener('install', (event) => {
-//   event.waitUntil(cacheHelper.cachingAppShell([...assets, './']))
-// })
+self.addEventListener('install', (event) => {
+  event.waitUntil(cacheHelper.cachingAppShell([
+    ...assets,
+    './',
+    './images/hero-image_1-large.webp',
+    './images/hero-image_1-small.webp',
+    './images/hero-image_1-large.jpg',
+    './images/hero-image_1-small.jpg'
+  ]))
+})
 
-// self.addEventListener('activate', (event) => {
-//   event.waitUntil(cacheHelper.deleteOldCache())
-// })
+self.addEventListener('activate', (event) => {
+  event.waitUntil(cacheHelper.deleteOldCache())
+})
 
-// self.addEventListener('fetch', (event) => {
-//   event.respondWith(cacheHelper.revalidateCache(event.request))
-// })
+self.addEventListener('fetch', (event) => {
+  event.respondWith(cacheHelper.revalidateCache(event.request))
+})
